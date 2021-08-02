03a66e23d5ada8e5f17be22e68570fa293e5fee1
hokyunrhee <hokyun.rhee@gmail.com>
2021-08-02 22:56:29 +0900

    build: ignore eslint during build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   webpack5: false,
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
