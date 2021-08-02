9c9dee71aed9027e34af2d058d19678056c7003e
hokyunrhee <hokyun.rhee@gmail.com>
2021-08-02 22:59:44 +0900

    feat: add chakra, query-client provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from 'styles/theme';
+
+// Create a client
+const queryClient = new QueryClient();
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
-
-export default MyApp;
