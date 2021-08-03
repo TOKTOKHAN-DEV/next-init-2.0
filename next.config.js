9f440abbe79423b28e9ea20ecf6b5aac0ebf0b0a
hokyunrhee <hokyun.rhee@gmail.com>
2021-08-03 10:38:51 +0900

    chore: disable x-powered-by

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   webpack5: false,
+  poweredByHeader: false,
   eslint: {
     // Warning: Dangerously allow production builds to successfully complete even if
     // your project has ESLint errors.
