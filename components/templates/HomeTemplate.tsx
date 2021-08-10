ed10c11bcd652508e980ef4ec034791e89e1cdd2
Jangwon <wkddnjset@gmail.com>
2021-08-10 13:44:44 +0900

    [Add] Button

--- components/templates/HomeTemplate.tsx
+++ components/templates/HomeTemplate.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
-import { Box } from '@chakra-ui/react';
+import { Box, Stack, Button } from '@chakra-ui/react';
 
 interface HomeTemplateProps {
   header: JSX.Element;
 }
 
 const HomeTemplate = ({ header }: HomeTemplateProps) => {
-  return <Box flexGrow={1}>{header}</Box>;
+  return (
+    <>
+      <Box flexGrow={1}>{header}</Box>
+      <Stack spacing={4} direction="row" align="center">
+        <Button colorScheme="teal" size="xs" borderRadius="50px">
+          Button
+        </Button>
+        <Button colorScheme="teal" size="sm" borderRadius="50px">
+          Button
+        </Button>
+        <Button colorScheme="teal" size="md" borderRadius="50px">
+          Button
+        </Button>
+        <Button colorScheme="teal" size="lg" borderRadius="50px">
+          Button
+        </Button>
+      </Stack>
+    </>
+  );
 };
 
 export default HomeTemplate;
