ef9aa9a443170c9196768d9f75d901be8a8ccb35
hokyunrhee <hokyun.rhee@gmail.com>
2021-08-10 23:13:45 +0900

    refactor: change breakpoints

--- styles/theme/foundations/breakpoints.ts
+++ styles/theme/foundations/breakpoints.ts
@@ -1,11 +1,12 @@
 import { createBreakpoints } from '@chakra-ui/theme-tools';
 
 const breakpoints = createBreakpoints({
-  sm: '480px',
-  md: '780px',
-  lg: '980px',
-  xl: '1280px',
-  '2xl': '1920px',
+  // base <780px
+  sm: '780px', // ≥780px
+  md: '980px', // ≥980px
+  lg: '1280', // ≥1280px
+  xl: '1536px', // ≥1536px
+  '2xl': '1920px', // ≥1920px
 });
 
 export default breakpoints;
