d6d279becb9c8448a00faa6cef5d9398d4f604be
Jangwon <wkddnjset@gmail.com>
2021-11-02 10:53:15 +0900

    .

--- containers/login/index.tsx
+++ containers/login/index.tsx
@@ -8,5 +8,5 @@ export const LoginContainer = () => {
   // TODO : 클라이언트 별로 Type 설정
   // type: 'social' | 'email' | 'email-with-social' | 'phone';
 
-  return <LoginTemplate content={<Content type="email-with-social" />} />;
+  return <LoginTemplate content={<Content type="email" />} />;
 };
