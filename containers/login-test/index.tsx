143e2267201bf145c8d616fac2a494eb1d79f9e7
Jangwon <wkddnjset@gmail.com>
2021-11-16 19:51:23 +0900

    feat: add test page

---
+++ containers/login-test/index.tsx
@@ -0,0 +1,10 @@
+import HomeTemplate from 'components/templates/HomeTemplate';
+
+export function HomeContainer() {
+  return (
+    <>
+      <h1>로그인 테스트 페이지 입니다.</h1>
+      <HomeTemplate />
+    </>
+  );
+}
---
+++ pages/login-test.tsx
@@ -0,0 +1,1 @@
+export { HomeContainer as default } from 'containers/login-test';
