d09bf6c6f7d523c07081a37711ea96987dc46ea0
Jangwon <wkddnjset@gmail.com>
2022-01-21 01:29:08 +0900

    feat: 🎸 add validate

---
+++ utils/validate.ts
@@ -0,0 +1,24 @@
+export const emailValidate = {
+  confirm: (value: string) => {
+    const reg = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    return reg.test(value) ? null : '이메일형식을 확인해주세요.';
+  },
+};
+
+export const passwordValidate = {
+  hasNumber: (value: string) => {
+    const reg = /[0-9]/;
+    return reg.test(value) ? null : '숫자가 포함되어야 합니다.';
+  },
+  hasText: (value: string) => {
+    const reg = /[a-zA-Z]/;
+    return reg.test(value) ? null : '대문자&소문자가 포함되어야 합니다.';
+  },
+  hasSpecial: (value: string) => {
+    const reg = /[~!@#$%^&*()_+|<>?:{}]/;
+    return reg.test(value) ? null : '특수문자가 포함되어야 합니다.';
+  },
+  length: (value: string) => {
+    return value.length > 8 ? null : '8자리 이상이어야 합니다.';
+  },
+};
