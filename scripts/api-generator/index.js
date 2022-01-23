a1cdf84a77324697f028a8bccf0060198cf9ec9e
ldu1020 <ldu1020@gmail.com>
2022-01-23 11:38:47 +0900

    feat(scripts): 🎸 add prompt on script of api-gen

--- scripts/api-generator/index.js
+++ scripts/api-generator/index.js
@@ -1,45 +1,29 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const generator = require('swagger-typescript-api');
-const path = require('path');
 const fs = require('fs');
-const { mkdir } = require('./src/utils/mkdir');
+const generator = require('swagger-typescript-api');
+const init = require('./src/init');
 
+const { mkdir } = require('./src/utils/mkdir');
+const { getPathOnApp } = require('./src/utils/path');
 const { generateApi } = generator;
 
-const TARGET = 'ficl.yaml';
+const customTemplatesFolder = getPathOnApp('src/template/custom-templates');
 
-const ROOT_PATH = process.env.PWD;
-const SCRIPT_PATH = path.resolve(ROOT_PATH, 'scripts');
-const SOURCE_PATH = path.resolve(SCRIPT_PATH, 'api-generator/src');
+async function main() {
+  const { outputFolder, targetSwaggerFile } = await init();
 
-const SWAGGER_PATH = path.resolve(SOURCE_PATH, 'swagger');
-const TEMPLATE_PATH = path.resolve(SOURCE_PATH, 'template');
-
-const CUSTOM_TEMPLATE_PATH = path.resolve(TEMPLATE_PATH, 'custom-templates');
-const TARGET_PATH = path.resolve(SWAGGER_PATH, TARGET);
-
-const [fileName] = TARGET.split('.');
-
-const ROOT_PATH_OF_GENERATED = path.resolve(ROOT_PATH, 'generated');
-mkdir(ROOT_PATH_OF_GENERATED);
-const API_PATH_GENERATED = path.resolve(ROOT_PATH_OF_GENERATED, 'api');
-mkdir(API_PATH_GENERATED);
-const OUTPUT_PATH = path.resolve(API_PATH_GENERATED, fileName);
-mkdir(OUTPUT_PATH);
-
-(async function () {
   const { files } = await generateApi({
-    input: TARGET_PATH,
+    input: targetSwaggerFile,
     httpClientType: 'axios', // or "fetch"
     modular: true,
     moduleNameFirstTag: true,
     typeSuffix: 'Type',
-    templates: CUSTOM_TEMPLATE_PATH,
+    templates: customTemplatesFolder,
   });
 
   files.forEach(({ name, content }) => {
     const folderName = name.replace('.ts', '');
-    const folderPath = `${OUTPUT_PATH}/${folderName}`;
+    const folderPath = `${outputFolder}/${folderName}`;
     const filePath = `${folderPath}/index.ts`;
     try {
       mkdir(folderPath);
@@ -48,4 +32,6 @@ mkdir(OUTPUT_PATH);
       console.error(err);
     }
   });
-})();
+}
+
+main();
