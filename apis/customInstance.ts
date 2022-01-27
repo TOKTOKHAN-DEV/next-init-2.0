a957bf91e9fc71a999e8ffca5d5d32480d80d0a7
devnine99 <devnine99@gmail.com>
2022-01-27 18:36:39 +0900

    fix: tslint

--- apis/customInstance.ts
+++ apis/customInstance.ts
@@ -1,34 +1,28 @@
-import Axios, { AxiosError, AxiosRequestConfig, AxiosRequestHeaders } from 'axios'
-import { BASE_URL } from 'config'
+import Axios, { AxiosError, AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
+import { BASE_URL } from 'config';
 
+export const AXIOS_INSTANCE = Axios.create({ baseURL: BASE_URL });
 
-export const AXIOS_INSTANCE = Axios.create({ baseURL: BASE_URL })
-
-export const useCustomInstance = <T>(): ((
-  config: AxiosRequestConfig,
-) => Promise<T>) => {
+export const useCustomInstance = <T>(): ((config: AxiosRequestConfig) => Promise<T>) => {
   // const token = useToken() // Do what you want
-  const token = ''
+  const token = '';
 
   return (config: AxiosRequestConfig) => {
-    const source = Axios.CancelToken.source()
-    const headers: AxiosRequestHeaders = {}
-    if (!!token) headers.Authorazation = `Bearer ${token}`
-    const promise = AXIOS_INSTANCE({
+    const source = Axios.CancelToken.source();
+    const headers: AxiosRequestHeaders = {};
+    if (token) headers.Authorazation = `Bearer ${token}`;
+    // promise.cancel = () => {
+    //   source.cancel('Query was cancelled by React Query');
+    // };
+
+    return AXIOS_INSTANCE({
       ...config,
       headers,
       cancelToken: source.token,
-    }).then(({ data }) => data)
+    }).then(({ data }) => data);
+  };
+};
 
-    // @ts-ignore
-    promise.cancel = () => {
-      source.cancel('Query was cancelled by React Query')
-    }
-
-    return promise
-  }
-}
-
-export default useCustomInstance
+export default useCustomInstance;
 
 export type ErrorType<Error> = AxiosError<Error>;
