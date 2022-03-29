b5cb219325de95d63c9e74fc669302c1d3fd82b4
ldu1020 <ldu1020@gmail.com>
2022-03-29 14:27:57 +0900

    fix(color-generator): 🐛 add comment, and fix logic

--- src/utils/color-generator/getColorSchema.ts
+++ src/utils/color-generator/getColorSchema.ts
@@ -1,26 +1,42 @@
 import { getColorToneSafety } from './getColorToneSafety';
 
 type ColorKey = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+type Options = {
+  keys?: ColorKey[];
+  percent?: number;
+};
 
-export function getColorSchema(
-  color: string,
-  keys: ColorKey[] = [
-    50, //
-    100,
-    200,
-    300,
-    400,
-    500,
-    600,
-    700,
-    800,
-    900,
-  ],
-) {
+/**
+ * target color 를 기준으로 밝기를 조절하여 chakra 의 color schema 형태로 반환하는 함수입니다.
+ * @param color - 대상 색상
+ * @param {Array} [options.keys = [50,100,200,300,400,500,600,700,800,900]]
+ *  - 가져올 색상을 결정합니다. Default value: [50, 100, ...900].
+ *  - ex) [500, 600, 700] = {500: #aaaaaa , 600: #bbbbbb, 700: #cccccc}
+ * @param {number} [options.percent = 5]
+ * - 기본컬러 부터(500)의 색상 밝기 간격을 결정합니다.
+ * - ex) percent 가 5% 일때 400 은 밝기가  500 에서 +5% 된 색상이고, 300 컬러는 +10% 색상입니다.
+ */
+export function getColorSchema(color: string, options?: Options) {
+  const {
+    percent = 5,
+    keys = [
+      50, //
+      100,
+      200,
+      300,
+      400,
+      500,
+      600,
+      700,
+      800,
+      900,
+    ],
+  } = options || {};
+
   const { darkTones, lightTones } = getColorToneSafety({
     color, //
-    percent: 5,
-    safetyLength: { light: 6, dark: 6 },
+    percent,
+    safetyLength: getSafetyLength(keys),
   });
 
   const colorMap = {
@@ -37,7 +53,45 @@ export function getColorSchema(
   };
 
   const colorSchema: Partial<Record<ColorKey, string>> = {};
-  for (const key of keys) colorSchema[key] = colorMap[key] || 'black';
+  for (const key of keys) colorSchema[key] = colorMap[key];
 
   return colorSchema;
+}
+
+function getSafetyLength(keys: ColorKey[]): { light: number; dark: number } {
+  // 키 별 꼭 받아와야하는 부터의 색상 갯수 ex) [50] = [500, 400, 300, 200, 100, 50]  총  6개 필요
+  const mappingLightColorSafetyLength = {
+    50: 6,
+    100: 5,
+    200: 4,
+    300: 3,
+    400: 2,
+  };
+
+  const mappingDarkColorSafetyLength = {
+    600: 2,
+    700: 3,
+    800: 4,
+    900: 5,
+  };
+
+  const safetyLengthMap = {
+    500: 1,
+    ...mappingDarkColorSafetyLength,
+    ...mappingLightColorSafetyLength,
+  };
+
+  const safetyLength = keys.reduce(
+    // prev = 초깃값 :  { dark: 0, light: 0 }
+    // cur =  key  값 : 100, 200, 300 ...
+    (prev, cur) => {
+      const updated = { ...prev };
+      if (cur < 500) updated.dark = Math.max(safetyLengthMap[cur], prev.dark);
+      if (cur > 500) updated.light = Math.max(safetyLengthMap[cur], prev.light);
+      return updated;
+    },
+    { dark: 0, light: 0 },
+  );
+
+  return safetyLength;
 }
--- src/utils/color-generator/getColorTone.ts
+++ src/utils/color-generator/getColorTone.ts
@@ -1,33 +1,40 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 import tinycolor from 'tinycolor2';
 
+/**
+ * target color 를 기준으로 밝기를 조절하여 밝은 색상 배열, 어두운 색상 배열을 리턴합니다.
+ * @param {string} color - 대상 색상
+ * @param {number} [options.percent = 5]
+ * - target color 부터 색상 밝기 간격을 결정합니다.
+ * @return target color 부터 점점 어두워지는 darkTones 배열과, 점점 밝아지는 lightTones 배열을 리턴합니다.
+ */
 export function getColorTone(color: string, percent = 5): { darkTones: string[]; lightTones: string[] } {
   const t = tinycolor(color);
-  const hsl = t.toHsl(); // { h: 0, s:1, l: 0.5 }
-
+  // hsl(hue, saturation, lightness)
+  const hsl = t.toHsl();
+  // 색조
   const colorH = hsl['h'];
+  // 채도 (0이면 회색, 100이면 원색)
   const colorS = hsl['s'] * 100;
+  // 밝기 (0이면 검정색 100이면 흰색)
   const colorL = hsl['l'] * 100;
 
   const lightTones: string[] = [];
   const darkTones: string[] = [];
 
   for (let i = 0; i < 100; i = i + percent) {
+    // 기존 밝기에서 (percent * n)% 만큼 올린값
     const lighten = colorL + i;
     const lightColor = tinycolor('hsl(' + colorH + ', ' + colorS + '%, ' + lighten + '%)');
-    const lightHex = lightColor.toHexString(); // 見本の色とカラーコード表示に
+    const lightHex = lightColor.toHexString();
 
+    // 기존 밝기에서 (percent * n)%만큼 내린 값
     const darken = colorL - i;
     const darkColor = tinycolor('hsl(' + colorH + ', ' + colorS + '%, ' + darken + '%)');
-    const darkHex = darkColor.toHexString(); // 見本の色とカラーコード表示に
+    const darkHex = darkColor.toHexString();
 
-    if (lighten <= 100) {
-      lightTones.push(lightHex);
-    }
-
-    if (darken >= 0) {
-      darkTones.push(darkHex);
-    }
+    if (lighten <= 100) lightTones.push(lightHex);
+    if (darken >= 0) darkTones.push(darkHex);
   }
 
   return { lightTones, darkTones };
