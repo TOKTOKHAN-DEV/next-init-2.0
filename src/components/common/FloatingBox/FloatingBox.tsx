a09b27418964fa1520fbeba8cfe88cbbcff53b0d
ldu1020 <ldu1020@gmail.com>
2022-04-22 15:07:25 +0900

    fix(FloatingBox): 🐛 naming

---
+++ src/components/common/FloatingBox/FloatingBox.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react';
+
+interface FloatingBoxProps {
+  position: 'top' | 'bottom';
+  children: JSX.Element;
+}
+const FloatingBox = ({ position, children }: FloatingBoxProps) => {
+  return (
+    <Box position="absolute" bottom={position === 'bottom' ? '30px' : 'none'} top={position === 'top' ? '30px' : 'none'} right="16px" zIndex="999">
+      {children}
+    </Box>
+  );
+};
+
+export default FloatingBox;
--- src/components/common/FloatingBox/index.tsx
+++ src/components/common/FloatingBox/index.tsx
@@ -1,15 +1,1 @@
-import { Box } from '@chakra-ui/react';
-
-interface Props {
-  position: 'top' | 'bottom';
-  children: JSX.Element;
-}
-const FloatingBox = ({ position, children }: Props) => {
-  return (
-    <Box position="absolute" bottom={position === 'bottom' ? '30px' : 'none'} top={position === 'top' ? '30px' : 'none'} right="16px" zIndex="999">
-      {children}
-    </Box>
-  );
-};
-
-export default FloatingBox;
+export { default } from './FloatingBox';
