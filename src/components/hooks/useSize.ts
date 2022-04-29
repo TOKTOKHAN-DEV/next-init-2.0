fa9efeac66bb8c0a9e0016a0b99381a950c5c8c9
Jangwon <wkddnjset@gmail.com>
2022-04-29 19:42:18 +0900

    feat: 🎸 add useSize -> get window size

---
+++ src/components/hooks/useSize.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+function useSize() {
+  const [height, setHeight] = useState<number>(0);
+  const [width, setWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const updateSize = () => {
+      if (window.innerWidth > 1920) {
+        const ratio = window.innerWidth / 1920;
+        setHeight(window.innerHeight / ratio);
+        setWidth(window.innerWidth / ratio);
+      } else {
+        setHeight(window.innerHeight);
+        setWidth(window.innerWidth);
+      }
+    };
+    updateSize();
+    window.addEventListener('resize', () => updateSize());
+    return () => window.removeEventListener('resize', () => updateSize());
+  }, []);
+
+  return { height, width };
+}
+
+export default useSize;
