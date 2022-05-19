d40aac2365c59977c1388a8a152fd690d20caa35
ldu1020 <ldu1020@gmail.com>
2022-05-19 12:12:27 +0900

    fix(theme): 🐛 file-name

---
+++ src/styles/theme/components/single/Container.ts
@@ -0,0 +1,10 @@
+import { ComponentSingleStyleConfig } from '@chakra-ui/theme';
+
+export const Container: ComponentSingleStyleConfig = {
+  baseStyle: {
+    maxW: ['100%', '780px', '980px', '1280px', '1480px', '1780px'],
+  },
+  defaultProps: {},
+  sizes: {},
+  variants: {},
+};
