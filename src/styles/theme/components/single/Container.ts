cfa36a99d8844a50e11b6a253f1cdc19ef0bfa9b
Jangwon <wkddnjset@gmail.com>
2022-09-27 11:56:19 +0900

    Update Container.ts

--- src/styles/theme/components/single/Container.ts
+++ src/styles/theme/components/single/Container.ts
@@ -2,7 +2,14 @@ import { ComponentSingleStyleConfig } from '@chakra-ui/theme';
 
 export const Container: ComponentSingleStyleConfig = {
   baseStyle: {
-    maxW: ['100%', '780px', '980px', '1280px', '1480px', '1780px'],
+    maxW: {
+      base: '100%',
+      sm: '780px',
+      md: '980px',
+      lg: '1280px',
+      xl: '1480px',
+      '2xl': '1780px',
+    },
   },
   defaultProps: {},
   sizes: {},
