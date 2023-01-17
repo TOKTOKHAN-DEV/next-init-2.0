fdc6a250d10856732f877b7a6cc9823b6c0ccbb4
Jangwon <wkddnjset@gmail.com>
2023-01-17 23:24:52 +0900

    Update textStyles.ts
    디자인 시스템 텍스트 스타일 추가

--- src/styles/theme/textStyles.ts
+++ src/styles/theme/textStyles.ts
@@ -1,4 +1,49 @@
 const textStyles = {
+  "ExtraLarge": {
+    fontSize: { base: '26px', sm: '26px', md: '35px' },
+    lineHeight: { base: '38px', sm: '38px', md: '52px' },
+  },
+  "TitleLarge": {
+    fontSize: { base: '20px', sm: '20px', md: '26px' },
+    fontWeight: "bold",
+    lineHeight: { base: '29px', sm: '29px', md: '42px' },
+  },
+  "Title": {
+    fontSize: { base: '16px', sm: '14px', md: '15px' },
+    fontWeight: "bold",
+    lineHeight: { base: '28px', sm: '26px', md: '27px' },
+  },
+  "TextLarge": {
+    fontSize: { base: '20px', sm: '20px', md: '26px' },
+    lineHeight: { base: '29px', sm: '29px', md: '42px' },
+  },
+  "Text": {
+    fontSize: { base: '16px', sm: '14px', md: '15px' },
+    lineHeight: { base: '28px', sm: '26px', md: '27px' },
+  },
+  "TextSmall": {
+    fontSize: { base: '12px', sm: '10px', md: '12px' },
+    lineHeight: { base: '18px', sm: '16px', md: '18px' },
+  },
+  "Button": {
+    fontSize: { base: '16px', sm: '14px', md: '15px' },
+    fontWeight: "bold",
+    lineHeight: { base: '28px', sm: '26px', md: '27px' },
+  },
+  "ButtonSmall": {
+    fontSize: { base: '12px', sm: '10px', md: '12px' },
+    lineHeight: { base: '18px', sm: '16px', md: '18px' },
+  },
+  "TextActive": {
+    fontSize: { base: '16px', sm: '14px', md: '15px' },
+    textDecoration: "underline",
+    lineHeight: { base: '28px', sm: '26px', md: '27px' },
+  },
+  "TextActiveSmall": {
+    fontSize: { base: '12px', sm: '10px', md: '12px' },
+    textDecoration: "underline",
+    lineHeight: { base: '18px', sm: '16px', md: '18px' },
+  },
   sm: {
     fontSize: ['12px', '10px', '12px'],
     lineHeight: ['18px', '16px', '18px'],
