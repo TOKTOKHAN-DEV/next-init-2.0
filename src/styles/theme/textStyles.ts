c7f39e0b7ef2a3fb32fb6690a346f4a9004dd609
ldu1020 <ldu1020@gmail.com>
2023-04-20 21:58:49 +0900

    fix(textStyles.ts): 🐛CamelCalse -> kebob-case

--- src/styles/theme/textStyles.ts
+++ src/styles/theme/textStyles.ts
@@ -1,50 +1,50 @@
 const textStyles = {
-  ExtraLarge: {
+  'extra-lg': {
     fontSize: { base: '26px', sm: '26px', md: '35px' },
     fontWeight: 700,
     lineHeight: { base: '38px', sm: '38px', md: '52px' },
   },
-  TitleLarge: {
+  'title-lg': {
     fontSize: { base: '20px', sm: '20px', md: '26px' },
     fontWeight: 700,
     lineHeight: { base: '29px', sm: '29px', md: '42px' },
   },
-  Title: {
+  title: {
     fontSize: { base: '16px', sm: '14px', md: '15px' },
     fontWeight: 700,
     lineHeight: { base: '28px', sm: '26px', md: '27px' },
   },
-  TextLarge: {
+  'text-lg': {
     fontSize: { base: '20px', sm: '20px', md: '26px' },
     fontWeight: 400,
     lineHeight: { base: '29px', sm: '29px', md: '42px' },
   },
-  Text: {
+  text: {
     fontSize: { base: '16px', sm: '14px', md: '15px' },
     fontWeight: 400,
     lineHeight: { base: '28px', sm: '26px', md: '27px' },
   },
-  TextSmall: {
+  'text-sm': {
     fontSize: { base: '12px', sm: '10px', md: '12px' },
     fontWeight: 400,
     lineHeight: { base: '18px', sm: '16px', md: '18px' },
   },
-  Button: {
+  btn: {
     fontSize: { base: '16px', sm: '14px', md: '15px' },
     fontWeight: 700,
     lineHeight: { base: '28px', sm: '26px', md: '27px' },
   },
-  ButtonSmall: {
+  'btm-sm': {
     fontSize: { base: '12px', sm: '10px', md: '12px' },
     fontWeight: 700,
     lineHeight: { base: '18px', sm: '16px', md: '18px' },
   },
-  TextActive: {
+  'text-active': {
     fontSize: { base: '16px', sm: '14px', md: '15px' },
     textDecoration: 'underline',
     lineHeight: { base: '28px', sm: '26px', md: '27px' },
   },
-  TextActiveSmall: {
+  'text-active-sm': {
     fontSize: { base: '12px', sm: '10px', md: '12px' },
     fontWeight: 400,
     textDecoration: 'underline',
