d560b9903a9a05fbbbb6e1faeddcf68dcf099d08
zeromountain <sonyeongsan@gmail.com>
2023-06-01 18:47:06 +0900

    feat: 🎸add mutable type for json format

---
+++ src/types/utility/deep-mutable.ts
@@ -0,0 +1,17 @@
+import { Mutable } from './mutable';
+
+// type Example = DeepMutable<{
+//   readonly a: number;
+//   readonly b: { readonly c: number; readonly d: string };
+// }>;
+
+// Example = {
+//   a: number;
+//   b?: { c? : number; d? : string };
+// }
+
+export type DeepMutable<T extends Record<any, any> | undefined> = Mutable<{
+  [K in keyof T]: T[K] extends Record<any, any> | undefined
+    ? Mutable<T[K]>
+    : T[K];
+}>;
