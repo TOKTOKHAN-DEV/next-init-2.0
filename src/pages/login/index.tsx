ef1941689e1aba7eb6ea73924a4b7d2ddf474b3d
keemtj <keemgreat@gmail.com>
2023-07-07 14:11:48 +0900

    docs(pages): ✏️Add comments above NextSeo component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,6 +7,8 @@ import withUnAuthGuard from '@/hocs/withUnAuthGuard';
 function LoginPage() {
   return (
     <>
+      {/* output: 똑똑한 개발자 | login */}
+      {/* titleTemplate는 /configs/seo/config.ts에서 변경 가능합니다. */}
       <NextSeo title="login" />
       <HomeLayout content={<Login />} />
     </>
