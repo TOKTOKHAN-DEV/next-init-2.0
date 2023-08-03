09df330125b5331603d1607447645984807ea8ca
devnine99 <46771072+devnine99@users.noreply.github.com>
2023-08-03 11:19:26 +0900

    update next image setting

--- next.config.js
+++ next.config.js
@@ -38,4 +38,20 @@ module.exports = withBundleAnalyzer({
       },
     ];
   },
-});
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '#{PROJECT_NAME}-dev-bucket.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/_media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '#{PROJECT_NAME}-prod-bucket.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/_media/**',
+      },
+    ],
+  },
+};
