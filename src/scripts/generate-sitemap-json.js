7d1d98ca6dc1abe910b70c9ab25043f4a289da04
AlgoRoots <algoroot524@gmail.com>
2023-08-04 22:48:06 +0900

    fix(generate-sitemap-json.js): 🐛use dynamic import

--- src/scripts/generate-sitemap-json.js
+++ src/scripts/generate-sitemap-json.js
@@ -1,34 +1,37 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require('fs');
-
-const globby = require('globby');
-
-// sitemap 생성시 제외할 라우트의 목록입니다.
-const FORBIDDEN = ['/signup', '/login'].map((route) => `!**${route}`);
 
 (async () => {
-  /**
-   * localRoutes: pages 폴더 내 파일을 기준으로 만듭니다.
-   */
-  const localFiles = await globby([
-    'src/pages/**/*.{js,tsx}',
-    '!src/pages/_*.{js,tsx}',
-    '!src/pages/**/[*.{js,tsx}',
-    '!src/pages/api',
-    ...FORBIDDEN,
-  ]);
+  const fs = await import('fs');
 
-  const localRoutes = localFiles
-    .map((file) => {
-      const route = file
-        .replace('src/pages', '')
-        .replace(/.tsx|.js/, '')
-        .replace(/\/index/g, '');
-      return route;
-    })
-    .map((route) => `${route}`)
-    .sort();
+  const { globby } = await import('globby');
 
-  // write the sitemap.json
-  fs.writeFileSync('public/sitemap.json', JSON.stringify(localRoutes));
+  // sitemap 생성시 제외할 라우트의 목록입니다.
+  const FORBIDDEN = ['/signup', '/login'].map((route) => `!**${route}`);
+
+  (async () => {
+    /**
+     * localRoutes: pages 폴더 내 파일을 기준으로 만듭니다.
+     */
+    const localFiles = await globby([
+      'src/pages/**/*.{js,tsx}',
+      '!src/pages/_*.{js,tsx}',
+      '!src/pages/**/[*.{js,tsx}',
+      '!src/pages/api',
+      ...FORBIDDEN,
+    ]);
+
+    const localRoutes = localFiles
+      .map((file) => {
+        const route = file
+          .replace('src/pages', '')
+          .replace(/.tsx|.js/, '')
+          .replace(/\/index/g, '');
+        return route;
+      })
+      .map((route) => `${route}`)
+      .sort();
+
+    // write the sitemap.json
+    fs.writeFileSync('public/sitemap.json', JSON.stringify(localRoutes));
+  })();
 })();
