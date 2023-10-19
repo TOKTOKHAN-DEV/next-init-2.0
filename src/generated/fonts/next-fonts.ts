ce2edf8b1f51333e7f9d63a2d2649e1846e06ffe
Eunkyung-Son <sek9510@naver.com>
2023-10-19 17:55:02 +0900

    perf(next-fonts): ⚡️change to variable font (extension woff to woff2)

--- src/generated/fonts/next-fonts.ts
+++ src/generated/fonts/next-fonts.ts
@@ -3,47 +3,47 @@ import localFont from 'next/font/local';
 export const Pretendard = localFont({
   src: [
     {
-      path: '../../../public/fonts/pretendard/Pretendard-Thin.woff',
+      path: '../../../public/fonts/pretendard/PretendardVariable.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '../../../public/fonts/pretendard/Pretendard-ExtraLight.woff',
+      path: '../../../public/fonts/pretendard/PretendardVariable.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../../../public/fonts/pretendard/Pretendard-Light.woff',
+      path: '../../../public/fonts/pretendard/PretendardVariable.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../../../public/fonts/pretendard/Pretendard-Regular.woff',
+      path: '../../../public/fonts/pretendard/PretendardVariable.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../../public/fonts/pretendard/Pretendard-Medium.woff',
+      path: '../../../public/fonts/pretendard/PretendardVariable.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../../public/fonts/pretendard/Pretendard-SemiBold.woff',
+      path: '../../../public/fonts/pretendard/PretendardVariable.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../../../public/fonts/pretendard/Pretendard-Bold.woff',
+      path: '../../../public/fonts/pretendard/PretendardVariable.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../../../public/fonts/pretendard/Pretendard-ExtraBold.woff',
+      path: '../../../public/fonts/pretendard/PretendardVariable.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../../../public/fonts/pretendard/Pretendard-Black.woff',
+      path: '../../../public/fonts/pretendard/PretendardVariable.woff2',
       weight: '900',
       style: 'normal',
     },
