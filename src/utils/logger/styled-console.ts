7b137b61e506885f770ff74f6d86a4f44ddaf2a5
AlgoRoots <algoroot524@gmail.com>
2023-10-25 01:29:26 +0900

    feat(format-server-errors.ts): 🎸format and style console output for server errors

--- src/utils/logger/styled-console.ts
+++ src/utils/logger/styled-console.ts
@@ -7,6 +7,7 @@ export type StyledConsoleArgs = {
   data: unknown;
   topicColor?: CSSProperties['color'];
   method?: 'log' | 'warn' | 'error' | 'info';
+  errors?: string;
 };
 
 function styledConsole({
@@ -15,12 +16,16 @@ function styledConsole({
   data,
   topicColor = 'skyblue',
   method = 'log',
+  errors = '',
 }: StyledConsoleArgs) {
   const term_1 = `%c[${topic}]`;
   const term_1_style = [`color: ${topicColor}`, 'font-weight : bold'].join(';');
   const term_2 = `%c${title}`;
   const term_2_style = ['font-weight : bold'].join(';');
+  const error_msg = `%c${errors}`;
+  const error_style = [`color: ${topicColor}`, 'font-weight : bold'].join(';');
   console[method](`${term_1}${term_2}`, term_1_style, term_2_style, data);
+  console[method](`${error_msg}`, error_style);
 }
 
 export default styledConsole;
