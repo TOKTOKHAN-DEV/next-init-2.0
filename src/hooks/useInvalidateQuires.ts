b5081141da9b42dc65e942b9b96a263f7531dc1f
AlgoRoots <algoroot524@gmail.com>
2023-10-27 12:24:22 +0900

    refactor(useInvalidateQuires.ts): 💡refactoring hook

--- src/hooks/useInvalidateQuires.ts
+++ src/hooks/useInvalidateQuires.ts
@@ -5,24 +5,14 @@ import { QueryKey, useQueryClient } from '@tanstack/react-query';
 const useInvalidateQuires = () => {
   const queryClient = useQueryClient();
 
-  const single = useCallback(
-    (queryKey: QueryKey) => {
-      return queryClient.invalidateQueries(queryKey);
+  const invalidate = useCallback(
+    (...queryKeys: QueryKey[]) => {
+      queryKeys.forEach((queryKey) => queryClient.invalidateQueries(queryKey));
     },
     [queryClient],
   );
 
-  const multiple = useCallback(
-    (queryKeys: QueryKey[]) => {
-      const flatKeys = queryKeys.map((i) => i.concat());
-      flatKeys.map((queryKey) => {
-        return queryClient.invalidateQueries(queryKey);
-      });
-    },
-    [queryClient],
-  );
-
-  return { single, multiple };
+  return invalidate;
 };
 
 export default useInvalidateQuires;
